72463badec73f2ae619c79f480f5cad862ed56b2
Tolek Banan <tolek@banan.local>
2024-07-18 15:57:16 +0200

    first commit

---
+++ actions/user.action.ts
@@ -0,0 +1,13 @@
+"use server";
+import User from "@/modals/user.modal";
+import { connect } from "@/db";
+
+export async function createUser(user: any) {
+  try {
+    await connect();
+    const newUser = await User.create(user);
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    console.log(error);
+  }
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <div className="text-2xl">Home Page</div>
+    </div>
+  );
+}
